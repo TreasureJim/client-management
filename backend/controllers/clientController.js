97ae23cc23828d92b15cb00b5de30e476c08b782
TreasureJim <boreback.work@gmail.com>
2021-07-10 21:59:27 +0800

    work on database querying class

--- backend/controllers/clientController.js
+++ backend/controllers/clientController.js
@@ -3,16 +3,18 @@ const clientModel = new Model('client');
 
 async function getClient(req, res) {
     try {
-        const clientData = (await clientModel.select('*')).rows;
-        console.log(clientData);
+        const clientData = (await clientModel.select("client.*, manager_account.first_name as manager_first_name, manager_account.last_name as manager_last_name", undefined, "left join user_account manager_account on client.manager_id = manager_account.id")).rows;
         res.status(200).json(clientData);
-        // select * from client left join user_account on client.manager_id = user_account.user_id;
     }
     catch (err) {
         console.error(err);
         res.status(500);
     }
-} 
+}
+
+async function addClient(req, res) {
+    return Error("Unimplemented function");
+}
 
 module.exports = {
     getClient
--- backend/db/model.js
+++ backend/db/model.js
@@ -8,20 +8,53 @@ class Model {
 		this.pool.on('error', (err, client) => `Error, ${err}, on idle client${client}`);
 	}
 
-	async select(columns, clause=undefined, join=undefined) {
+	// select statement to table 
+	async select(columns = "*", clause, join) {
 		let query;
-		if (clause !== undefined) {
-			query = `SELECT ${columns} FROM ${this.table} WHERE ${clause}`;
+		if (join === undefined) {
+			// no join with clause
+			if (clause !== undefined) {
+				query = `SELECT ${columns} FROM ${this.table} WHERE ${clause}`;
+			}
+			// no join and no clause
+			else {
+				query = `SELECT ${columns} FROM ${this.table};`;
+			}
 		}
 		else {
-			query = `SELECT ${columns} FROM ${this.table};`;
+			// join with clause
+			if (clause !== undefined) {
+				query = `SELECT ${columns} FROM ${this.table} WHERE ${clause} ${join}`;
+			}
+			// join and no clause
+			else {
+				query = `SELECT ${columns} FROM ${this.table} ${join};`;
+			}
 		}
 
+		// console.log(query);
 		return this.pool.query(query);
 	}
 
+	// insert rows
+	async insert(columns, rows) {
+		let query;
+		if (columns === undefined) { query = `INSERT INTO ${this.table} VALUES `; }
+		else { query = `INSERT INTO ${this.table}(${columns}) VALUES `; }
+
+		rows.forEach((element, index, array) => {
+			query += `(${element})`;
+			if (index < array.lengh - 1) { query += ', '; }
+			else { query += ';'; }
+		});
+
+		console.log(query);
+		return this.pool.query(query);
+	}
+
+	// generic command
 	async command(command) {
-		let query = `SELECT ${command};`;
+		let query = `${command};`;
 		return this.pool.query(query);
 	}
 }
